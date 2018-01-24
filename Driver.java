13c3a2fe4693bbe8c71e2c58cb6e32066b497a85
Cole Polyak <colepolyak@gmail.com>
2018-01-23 20:33:14 -0700

    Driver, Class, and StdDraw.

---
+++ Driver.java
@@ -0,0 +1,12 @@
+import edu.princeton.cs.introcs.StdDraw;
+
+public class Driver {
+
+	public static void main(String[] args) {
+		
+		StdDraw.setPenColor(UnStdDraw.AGED_MOUSTACHE_GREY);
+		StdDraw.filledSquare(0.5, 0.5, 0.2);
+
+	}
+
+}
---
+++ UnStdDraw.java
@@ -0,0 +1,22 @@
+import edu.princeton.cs.introcs.StdDraw;
+import java.awt.Color;
+
+public class UnStdDraw {
+
+	public static Color DENIM_BLUE = new Color(34, 67, 182);
+	public static Color DARK_SALMON_INJUSTICE = new Color(233, 150, 122);
+	public static Color AGED_MOUSTACHE_GREY = new Color(126, 126, 126);
+
+	public static void filledRegularNgon(double centerX, double centerY, double radius, int n)
+	{
+		//Method goes here.
+	}
+
+	public static void spiral( double centerX, double centerY, double maxRadius, double spinRate, int numSegments)
+	{
+		//Method goes here.
+	}
+}
+
+
+
